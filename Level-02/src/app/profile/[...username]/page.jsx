6d650eba967ce0ef4341ae391ed2ce9cec654db5
ChinmayKaitade <chinmaykaitade123@gmail.com>
2026-07-22 21:43:57 +0530

    Level-02: Cache-All Route Segment code files added

--- Level-02/src/app/profile/[...username]/page.jsx
+++ Level-02/src/app/profile/[...username]/page.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 async function DynamicProfilePage({ params }) {
   const { username } = await params;
+  const data = await params;
+  console.log(data);
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
